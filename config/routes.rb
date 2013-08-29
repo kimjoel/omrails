443115c10321d25fc126d115edd101fed6453947
Joel Kim <joel@wtx.com.au>
2013-08-29 02:06:15 +0000

    made projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Omrails::Application.routes.draw do
+  resources :projects
+
   devise_for :users
   get 'about' => 'pages#about'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130828060246) do
+ActiveRecord::Schema.define(version: 20130829020324) do
+
+  create_table "projects", force: true do |t|
+    t.string   "description"
+    t.string   "string"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
